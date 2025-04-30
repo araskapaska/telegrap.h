e112cd68db8ccf7afbbd952bb349323f20e68f3b
araskapaska <paveltukhtin3@gmail.com>
2025-04-30 18:04:18 +0300

    Update log.js

--- js/log.js
+++ js/log.js
@@ -5,16 +5,15 @@ async function logVisitor(slug) {
     const ua = navigator.userAgent;
 
     const msg = `
-<pre>
-Ссылка: ${slug}
-IP: ${data.ip}
-Страна: ${data.country}
-Город: ${data.city}
-Координаты: ${data.latitude}, ${data.longitude}
-Провайдер: ${data.connection?.isp}
-Локальное время: ${data.timezone?.current_time}
-UA: ${ua}
-</pre>
+IP: \`\`\`${data.ip}\`\`\`
+Страна: \`\`\`${data.country}\`\`\`
+Город: \`\`\`${data.city}\`\`\`
+Координаты: \`\`\`${data.latitude}, ${data.longitude}\`\`\`
+Провайдер: \`\`\`${data.connection?.isp}\`\`\`
+Локальное время: \`\`\`${data.timezone?.current_time}\`\`\`
+Платформа: \`\`\`${navigator.platform}\`\`\`
+UA: \`\`\`${ua}\`\`\`
+Ссылка на статью: \`\`\`${slug}\`\`\`
 `;
 
     await fetch("https://api.telegram.org/bot7329999473:AAEglilZMhtE6Iyr_uhLLRlI-32cIROEmNY/sendMessage", {
@@ -23,7 +22,7 @@ UA: ${ua}
       body: JSON.stringify({
         chat_id: 2079893058,
         text: msg,
-        parse_mode: "HTML"
+        parse_mode: "Markdown"
       })
     });
   } catch (err) {
